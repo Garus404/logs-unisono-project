d435f7cfc4f42b75d47edd1d51e7d503e78a8166
BaLKon Мороз <morozbalkon@gmail.com>
2025-10-03 14:58:27 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve <dy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ['gamedig'],
+  serverExternalPackages: ['gamedig', 'keyv'],
   typescript: {
     ignoreBuildErrors: true,
   },
